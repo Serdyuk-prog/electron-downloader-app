244ea81d0c123a9fccdb4f674bd3c141289b34c0
Serdyuk-prog <tony.not.gloomy@gmail.com>
2022-10-26 20:14:38 +0700

    init

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+// import { Button } from '@mui/material';
+
+function App() {
+    return (
+        <>
+            <h1>Hello world</h1>
+        </>
+    );
+}
+
+export default App;
