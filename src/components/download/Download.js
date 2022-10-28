9cb68e7eac2760660f658b00ea138396534687a1
Serdyuk-prog <tony.not.gloomy@gmail.com>
2022-10-28 23:13:59 +0700

    interactive components

--- src/components/download/Download.js
+++ src/components/download/Download.js
@@ -1,10 +1,61 @@
 import { TextField, Button, Box, Divider } from "@mui/material";
-import Icon from '@mui/material/Icon';
-import IconButton from '@mui/material/IconButton';
+import Icon from "@mui/material/Icon";
+import IconButton from "@mui/material/IconButton";
+import { useState } from "react";
 
-function Download() {
+function Download(props) {
+    class BtnState {
+        constructor(variant, text, isDisabled) {
+            this.variant = variant;
+            this.text = text;
+            this.isDisabled = isDisabled;
+        }
+    }
+
+    class DownloadState {
+        constructor(name, btnState) {
+            this.name = name;
+            this.btnState = btnState;
+        }
+    }
+
+    const downloadStateOptions = {
+        ready: new DownloadState(
+            "ready",
+            new BtnState("contained", "Скачать", false)
+        ),
+        inProgress: new DownloadState(
+            "inProgress",
+            new BtnState("outlined", "Пауза", false)
+        ),
+        stopped: new DownloadState(
+            "stopped",
+            new BtnState("outlined", "Продолжить", false)
+        ),
+        done: new DownloadState(
+            "done",
+            new BtnState("contained", "Готово", true)
+        ),
+    };
+
+    const [downloadState, setDownloadState] = useState(
+        downloadStateOptions.ready
+    );
+    const [progPercent] = useState(0);
+
+    const clickDownloadHandler = () => {
+        if (downloadState.name === downloadStateOptions.ready.name) {
+            setDownloadState(downloadStateOptions.inProgress);
+            return;
+        }
+        if (downloadState.name === downloadStateOptions.inProgress.name) {
+            setDownloadState(downloadStateOptions.stopped);
+            return;
+        }
+    };
+
     return (
-        <Box sx={{mb: 3}} >
+        <Box id={props.id} sx={{ mb: 3 }}>
             <Box
                 sx={{ mb: 3, display: "flex", justifyContent: "space-between" }}
             >
@@ -14,8 +65,15 @@ function Download() {
                     variant="outlined"
                     sx={{ width: "40%" }}
                 />
-                <Button sx={{ alignSelf: "center" }} variant="contained">
-                    Button
+                <Button
+                    sx={{ alignSelf: "center" }}
+                    variant={downloadState.btnState.variant}
+                    onClick={() => {
+                        clickDownloadHandler();
+                    }}
+                    disabled={downloadState.btnState.isDisabled}
+                >
+                    {downloadState.btnState.text}
                 </Button>
                 <Box
                     sx={{
@@ -25,11 +83,10 @@ function Download() {
                         alignItems: "center",
                     }}
                 >
-                    <span>100%</span>
+                    <span>{`${progPercent}%`}</span>
                 </Box>
                 <Box
                     sx={{
-                        
                         display: "flex",
                         justifyContent: "center",
                         alignItems: "center",
@@ -38,7 +95,6 @@ function Download() {
                     <IconButton>
                         <Icon>cancel</Icon>
                     </IconButton>
-                    
                 </Box>
             </Box>
             <Divider />
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,12 +2,36 @@ import Download from "../components/download/Download";
 import DlPanel from "../components/download_panel/DlPanel";
 import { Button } from "@mui/material";
 import Icon from "@mui/material/Icon";
+import { useState } from "react";
 
 function HomePage() {
+    class DownloadEl {
+        constructor(id) {
+            this.id = id;
+        }
+    }
+
+    const [downloads, setDownloads] = useState([]);
+
+    const onAdd = () => {
+        let newId = 0;
+        if (downloads.length) {
+            newId = downloads[downloads.length - 1].id + 1;
+        }
+        setDownloads([...downloads, new DownloadEl(newId)]);
+    };
     return (
         <DlPanel>
-            <Download />
-            <Button sx={{ alignSelf: "center" }} variant="contained">
+            {downloads.map((download) => {
+                return <Download key={download.id} id={download.id} />;
+            })}
+            <Button
+                sx={{ alignSelf: "center" }}
+                variant="contained"
+                onClick={() => {
+                    onAdd();
+                }}
+            >
                 <Icon>add_circle</Icon>
             </Button>
         </DlPanel>
